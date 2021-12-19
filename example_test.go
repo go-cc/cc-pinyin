699e78da01700b683f30880398677eba95ff54f9
Tong Sun <suntong@cpan.org>
2021-12-19 15:03:10 -0500

    - [#] clean up and copy *_test.go from cc-pinyin-arch

--- example_test.go
+++ example_test.go
@@ -3,135 +3,56 @@ package pinyin_test
 import (
 	"fmt"
 
-	"github.com/mozillazg/go-pinyin"
+	pinyin "github.com/go-cc/cc-pinyin"
 )
 
-func ExampleConvert() {
-	hans := "中国人"
-	fmt.Println("default:", pinyin.Convert(hans, nil))
-	// Output: default: [[zhong] [guo] [ren]]
-}
+// for standalone test, change package to main and the next func def to,
+// func main() {
+func Example_output() {
+	s := `名著：《红楼梦》〖清〗曹雪芹 著、高鹗 续／『人民文学』出版社／1996—9月30日／59.70【元】，《三国演义》〖明〗罗贯中。`
+	_ = s
+	hans := "中国人的〖中国银行〗，很.行.。"
+	// Separator 默认配置：所用的分隔符
+	var Separator = " "
+	var a pinyin.Pinyin
 
-func ExamplePinyin_default() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	fmt.Println("default:", pinyin.Pinyin(hans, a))
-	// Output: default: [[zhong] [guo] [ren]]
-}
+	// 默认
+	a = pinyin.NewPinyin(pinyin.Normal, pinyin.Normal, Separator, false, false)
+	//a.Separator = "_"
+	fmt.Println(a.Convert(hans))
 
-func ExamplePinyin_normal() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.Normal
-	fmt.Println("Normal:", pinyin.Pinyin(hans, a))
-	// Output: Normal: [[zhong] [guo] [ren]]
-}
+	// 包含声调
+	a = pinyin.NewPinyin(pinyin.Tone3, pinyin.Normal, Separator, false, false)
+	fmt.Println(a.Convert(hans))
 
-func ExamplePinyin_tone() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.Tone
-	fmt.Println("Tone:", pinyin.Pinyin(hans, a))
-	// Output: Tone: [[zhōng] [guó] [rén]]
-}
+	// 声调用数字表示
+	a = pinyin.NewPinyin(pinyin.Tone2, pinyin.Normal, Separator, false, false)
+	fmt.Println(a.Convert(hans))
 
-func ExamplePinyin_tone2() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.Tone2
-	fmt.Println("Tone2:", pinyin.Pinyin(hans, a))
-	// Output: Tone2: [[zho1ng] [guo2] [re2n]]
-}
+	// 声调在拼音后用数字表示
+	a = pinyin.NewPinyin(pinyin.Tone1, pinyin.Normal, Separator, false, false)
+	fmt.Println(a.Convert(hans))
 
-func ExamplePinyin_initials() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.Initials
-	fmt.Println("Initials:", pinyin.Pinyin(hans, a))
-	// Output: Initials: [[zh] [g] [r]]
-}
+	// 开启多音字模式
+	a = pinyin.NewPinyin(pinyin.Tone1, pinyin.Normal, Separator, true, false)
+	fmt.Println(a.Convert(hans))
+	a = pinyin.NewPinyin(pinyin.Tone3, pinyin.Normal, Separator, true, false)
+	fmt.Println(a.Convert(hans))
 
-func ExamplePinyin_firstLetter() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.FirstLetter
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[z] [g] [r]]
-}
+	// 11: 双显风格，返回 汉字 + 拼音
+	a = pinyin.NewPinyin(pinyin.Tone3, pinyin.Both, Separator, false, true)
+	fmt.Println(a.Convert("中国银行。"))
 
-func ExamplePinyin_finals() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.Finals
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[ong] [uo] [en]]
-}
+	// Output:
+	// MingZhu：《HongLouMeng》〖Qing〗CaoXueQin Zhu、GaoZuo Xu／『RenMinWenXue』ChuBanShe／1996—9Yue30Ri／59.70【Yuan】，《SanGuoYanYi》〖Ming〗LuoGuanZhong。
+	// ming-zhu-：《hong-lou-meng-》〖qing-〗cao-xue-qin- zhu-、gao-zuo- xu-／『ren-min-wen-xue-』chu-ban-she-／1996—9yue-30ri-／59.70【yuan-】，《san-guo-yan-yi-》〖ming-〗luo-guan-zhong-。
 
-func ExamplePinyin_finalsTone() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.FinalsTone
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[ōng] [uó] [én]]
-}
-
-func ExamplePinyin_finalsTone2() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Style = pinyin.FinalsTone2
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[o1ng] [uo2] [e2n]]
-}
-
-func ExamplePinyin_heteronym() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	a.Heteronym = true
-	a.Style = pinyin.Tone2
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[zho1ng zho4ng] [guo2] [re2n]]
-}
-
-func ExamplePinyin_fallbackCustom1() {
-	hans := "中国人abc"
-	a := pinyin.NewArgs()
-	a.Fallback = func(r rune, a pinyin.Args) []string {
-		return []string{string(r)}
-	}
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[zhong] [guo] [ren] [a] [b] [c]]
-}
-
-func ExamplePinyin_fallbackCustom2() {
-	hans := "中国人アイウ"
-	a := pinyin.NewArgs()
-	a.Fallback = func(r rune, a pinyin.Args) []string {
-		data := map[rune][]string{
-			'ア': {"a"},
-			'イ': {"i"},
-			'ウ': {"u"},
-		}
-		s, ok := data[r]
-		if ok {
-			return s
-		} else {
-			return []string{}
-		}
-	}
-	fmt.Println(pinyin.Pinyin(hans, a))
-	// Output: [[zhong] [guo] [ren] [a] [i] [u]]
-}
-
-func ExampleLazyPinyin() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	fmt.Println(pinyin.LazyPinyin(hans, a))
-	// Output: [zhong guo ren]
-}
-
-func ExampleSlug() {
-	hans := "中国人"
-	a := pinyin.NewArgs()
-	fmt.Println(pinyin.Slug(hans, a))
-	// Output: zhong-guo-ren
+	// Output:
+	// zhong guo ren de 〖zhong guo yin xing 〗，hen .xing .。
+	// zhōng guó rén de 〖zhōng guó yín xíng 〗，hěn .xíng .。
+	// zho1ng guo2 re2n de 〖zho1ng guo2 yi2n xi2ng 〗，he3n .xi2ng .。
+	// zhong1 guo2 ren2 de 〖zhong1 guo2 yin2 xing2 〗，hen3 .xing2 .。
+	// zho1ng/zho4ng guo2 ren2 de/di4/di2 〖zho1ng/zho4ng guo2 yin2 xi2ng/ha2ng/xi4ng/ha4ng/he2ng 〗，hen3 .xi2ng/ha2ng/xi4ng/ha4ng/he2ng .。
+	// zhōng/zhòng guó rén de/dì/dí 〖zhōng/zhòng guó yín xíng/háng/xìng/hàng/héng 〗，hěn .xíng/háng/xìng/hàng/héng .。
+	// 中(Zhōng) 国(Guó) 银(Yín) 行(Xíng) 。
 }
--- pinyin_test.go
+++ pinyin_test.go
@@ -1,407 +1,219 @@
 package pinyin
 
 import (
-	"reflect"
 	"testing"
 )
 
-type pinyinFunc func(string, Args) [][]string
+type pinyinFunc func(string) [][]string
+
 type testCase struct {
-	args   Args
-	result [][]string
+	a      Pinyin
+	result string
 }
 
-func testPinyin(t *testing.T, s string, d []testCase, f pinyinFunc) {
+func testPinyin(t *testing.T, s string, d []testCase) {
 	for _, tc := range d {
-		v := f(s, tc.args)
-		if !reflect.DeepEqual(v, tc.result) {
-			t.Errorf("Expected %s, got %s", tc.result, v)
+		v := tc.a.Convert(s)
+		if v != tc.result {
+			t.Errorf(`Expected "%s", got "%s"`, tc.result, v)
 		}
 	}
 }
 
 func TestPinyin(t *testing.T) {
 	hans := "中国人"
+	Separator := " "
 	testData := []testCase{
 		// default
 		{
-			Args{Style: Normal},
-			[][]string{
-				{"zhong"},
-				{"guo"},
-				{"ren"},
-			},
+			NewPinyin(Normal, Normal, Separator, false, false),
+			"zhong guo ren ",
 		},
-		// default
 		{
-			NewArgs(),
-			[][]string{
-				{"zhong"},
-				{"guo"},
-				{"ren"},
-			},
-		},
-		// Normal
-		{
-			Args{Style: Normal},
-			[][]string{
-				{"zhong"},
-				{"guo"},
-				{"ren"},
-			},
-		},
-		// Tone
-		{
-			Args{Style: Tone},
-			[][]string{
-				{"zhōng"},
-				{"guó"},
-				{"rén"},
-			},
-		},
-		// Tone2
-		{
-			Args{Style: Tone2},
-			[][]string{
-				{"zho1ng"},
-				{"guo2"},
-				{"re2n"},
-			},
+			NewPinyin(Normal, Normal, Separator, false, true),
+			"Zhong Guo Ren ",
 		},
 		// Tone3
 		{
-			Args{Style: Tone3},
-			[][]string{
-				{"zhong1"},
-				{"guo2"},
-				{"ren2"},
-			},
+			NewPinyin(Tone3, Normal, Separator, false, false),
+			"zhōng guó rén ",
+		},
+		{
+			NewPinyin(Tone3, Normal, Separator, false, true),
+			"Zhōng Guó Rén ",
+		},
+		// Tone2
+		{
+			NewPinyin(Tone2, Normal, Separator, false, false),
+			"zho1ng guo2 re2n ",
+		},
+		{
+			NewPinyin(Tone2, Normal, Separator, false, true),
+			"Zho1ng Guo2 Re2n ",
+		},
+		// Tone1
+		{
+			NewPinyin(Tone1, Normal, Separator, false, false),
+			"zhong1 guo2 ren2 ",
+		},
+		{
+			NewPinyin(Tone1, Normal, Separator, false, true),
+			"Zhong1 Guo2 Ren2 ",
 		},
 		// Initials
 		{
-			Args{Style: Initials},
-			[][]string{
-				{"zh"},
-				{"g"},
-				{"r"},
-			},
+			NewPinyin(Normal, Initials, Separator, false, false),
+			"zh g r ",
+		},
+		{
+			NewPinyin(Normal, Initials, Separator, false, true),
+			"Zh G R ",
 		},
 		// FirstLetter
 		{
-			Args{Style: FirstLetter},
-			[][]string{
-				{"z"},
-				{"g"},
-				{"r"},
-			},
+			NewPinyin(Normal, FirstLetter, Separator, false, false),
+			"z g r ",
+		},
+		{
+			NewPinyin(Normal, FirstLetter, Separator, false, true),
+			"Z G R ",
 		},
 		// Finals
 		{
-			Args{Style: Finals},
-			[][]string{
-				{"ong"},
-				{"uo"},
-				{"en"},
-			},
+			NewPinyin(Normal, Finals, Separator, false, false),
+			"ong uo en ",
+		},
+		{
+			NewPinyin(Normal, Finals, Separator, false, true),
+			"Ong Uo En ",
 		},
 		// FinalsTone
 		{
-			Args{Style: FinalsTone},
-			[][]string{
-				{"ōng"},
-				{"uó"},
-				{"én"},
-			},
+			NewPinyin(Tone3, Finals, Separator, false, false),
+			"ōng uó én ",
+		},
+		{
+			NewPinyin(Tone3, Finals, Separator, false, true),
+			"Ōng Uó Én ",
 		},
 		// FinalsTone2
 		{
-			Args{Style: FinalsTone2},
-			[][]string{
-				{"o1ng"},
-				{"uo2"},
-				{"e2n"},
-			},
-		},
-		// FinalsTone3
-		{
-			Args{Style: FinalsTone3},
-			[][]string{
-				{"ong1"},
-				{"uo2"},
-				{"en2"},
-			},
-		},
-		// Heteronym
-		{
-			Args{Heteronym: true},
-			[][]string{
-				{"zhong", "zhong"},
-				{"guo"},
-				{"ren"},
-			},
-		},
-	}
-
-	testPinyin(t, hans, testData, Pinyin)
-
-	// 测试不是多音字的 Heteronym
-	hans = "你"
-	testData = []testCase{
-		{
-			Args{},
-			[][]string{
-				{"ni"},
-			},
+			NewPinyin(Tone2, Finals, Separator, false, false),
+			"o1ng uo2 e2n ",
 		},
 		{
-			Args{Heteronym: true},
-			[][]string{
-				{"ni"},
-			},
+			NewPinyin(Tone2, Finals, Separator, false, true),
+			"O1ng Uo2 E2n ",
 		},
-	}
-	testPinyin(t, hans, testData, Pinyin)
-}
-
-func TestNoneHans(t *testing.T) {
-	s := "abc"
-	v := Pinyin(s, NewArgs())
-	value := [][]string{}
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestLazyPinyin(t *testing.T) {
-	s := "中国人"
-	v := LazyPinyin(s, Args{})
-	value := []string{"zhong", "guo", "ren"}
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	s = "中国人abc"
-	v = LazyPinyin(s, Args{})
-	value = []string{"zhong", "guo", "ren"}
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestSlug(t *testing.T) {
-	s := "中国人"
-	v := Slug(s, Args{})
-	value := "zhongguoren"
-	if v != value {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	v = Slug(s, Args{Separator: ","})
-	value = "zhong,guo,ren"
-	if v != value {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	a := NewArgs()
-	v = Slug(s, a)
-	value = "zhong-guo-ren"
-	if v != value {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	s = "中国人abc，,中"
-	v = Slug(s, a)
-	value = "zhong-guo-ren-zhong"
-	if v != value {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestFinal(t *testing.T) {
-	value := "an"
-	v := final("an")
-	if v != value {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestFallback(t *testing.T) {
-	hans := "中国人abc"
-	testData := []testCase{
-		// default
+		// FinalsTone1
 		{
-			NewArgs(),
-			[][]string{
-				{"zhong"},
-				{"guo"},
-				{"ren"},
-			},
+			NewPinyin(Tone1, Finals, Separator, false, false),
+			"ong1 uo2 en2 ",
 		},
-		// custom
 		{
-			Args{
-				Fallback: func(r rune, a Args) []string {
-					return []string{"la"}
-				},
-			},
-			[][]string{
-				{"zhong"},
-				{"guo"},
-				{"ren"},
-				{"la"},
-				{"la"},
-				{"la"},
-			},
+			NewPinyin(Tone1, Finals, Separator, false, true),
+			"Ong1 Uo2 En2 ",
 		},
-		// custom
 		{
-			Args{
-				Heteronym: true,
-				Fallback: func(r rune, a Args) []string {
-					return []string{"la", "wo"}
-				},
-			},
-			[][]string{
-				{"zhong", "zhong"},
-				{"guo"},
-				{"ren"},
-				{"la", "wo"},
-				{"la", "wo"},
-				{"la", "wo"},
-			},
+			NewPinyin(Tone3, Both, Separator, false, true),
+			"中(Zhōng) 国(Guó) 人(Rén) ",
 		},
 	}
-	testPinyin(t, hans, testData, Pinyin)
+
+	testPinyin(t, hans, testData)
+
 }
 
 type testItem struct {
 	hans   string
-	args   Args
-	result [][]string
+	a      Pinyin
+	result string
 }
 
-func testPinyinUpdate(t *testing.T, d []testItem, f pinyinFunc) {
+func testPinyinUpdate(t *testing.T, d []testItem) {
 	for _, tc := range d {
-		v := f(tc.hans, tc.args)
-		if !reflect.DeepEqual(v, tc.result) {
-			t.Errorf("Expected %s, got %s", tc.result, v)
+		v := tc.a.Convert(tc.hans)
+		//t.Log(v)
+		if v != tc.result {
+			t.Errorf("'%s' expects '%s', got '%s'", tc.hans, tc.result, v)
 		}
 	}
 }
 
 func TestUpdated(t *testing.T) {
+	Separator := " "
 	testData := []testItem{
 		// 误把 yu 放到声母列表了
-		{"鱼", Args{Style: Tone2}, [][]string{{"yu2"}}},
-		{"鱼", Args{Style: Tone3}, [][]string{{"yu2"}}},
-		{"鱼", Args{Style: Finals}, [][]string{{"v"}}},
-		{"雨", Args{Style: Tone2}, [][]string{{"yu3"}}},
-		{"雨", Args{Style: Tone3}, [][]string{{"yu3"}}},
-		{"雨", Args{Style: Finals}, [][]string{{"v"}}},
-		{"元", Args{Style: Tone2}, [][]string{{"yua2n"}}},
-		{"元", Args{Style: Tone3}, [][]string{{"yuan2"}}},
-		{"元", Args{Style: Finals}, [][]string{{"van"}}},
+		{"鱼", NewPinyin(Tone2, Normal, Separator, false, false), "yu2 "},
+		{"鱼", NewPinyin(Tone1, Normal, Separator, false, false), "yu2 "},
+		{"鱼", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "v "},
+		{"雨", NewPinyin(Tone2, Normal, Separator, false, false), "yu3 "},
+		{"雨", NewPinyin(Tone1, Normal, Separator, false, false), "yu3 "},
+		{"雨", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "v "},
+		{"元", NewPinyin(Tone3, Normal, Separator, false, false), "yuán "},
+		{"元", NewPinyin(Tone2, Normal, Separator, false, false), "yua2n "},
+		{"元", NewPinyin(Tone1, Normal, Separator, false, false), "yuan2 "},
+		{"元", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "van "},
 		// y, w 也不是拼音, yu的韵母是v, yi的韵母是i, wu的韵母是u
-		{"呀", Args{Style: Initials}, [][]string{{""}}},
-		{"呀", Args{Style: Tone2}, [][]string{{"ya"}}},
-		{"呀", Args{Style: Tone3}, [][]string{{"ya"}}},
-		{"呀", Args{Style: Finals}, [][]string{{"ia"}}},
-		{"无", Args{Style: Initials}, [][]string{{""}}},
-		{"无", Args{Style: Tone2}, [][]string{{"wu2"}}},
-		{"无", Args{Style: Tone3}, [][]string{{"wu2"}}},
-		{"无", Args{Style: Finals}, [][]string{{"u"}}},
-		{"衣", Args{Style: Tone2}, [][]string{{"yi1"}}},
-		{"衣", Args{Style: Tone3}, [][]string{{"yi1"}}},
-		{"衣", Args{Style: Finals}, [][]string{{"i"}}},
-		{"万", Args{Style: Tone2}, [][]string{{"wa4n"}}},
-		{"万", Args{Style: Tone3}, [][]string{{"wan4"}}},
-		{"万", Args{Style: Finals}, [][]string{{"uan"}}},
+		// {"呀", NewPinyin(Normal, Initials, Separator, false, false), " "},
+		{"呀", NewPinyin(Tone2, Normal, Separator, false, false), "ya "},
+		{"呀", NewPinyin(Tone1, Normal, Separator, false, false), "ya "},
+		// {"呀", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "ia "},
+		// {"无", NewPinyin(Normal, Initials, Separator, false, false), " "},
+		{"无", NewPinyin(Tone2, Normal, Separator, false, false), "wu2 "},
+		{"无", NewPinyin(Tone1, Normal, Separator, false, false), "wu2 "},
+		{"无", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "u "},
+		{"衣", NewPinyin(Tone2, Normal, Separator, false, false), "yi1 "},
+		{"衣", NewPinyin(Tone1, Normal, Separator, false, false), "yi1 "},
+		{"衣", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "i "},
+		{"万", NewPinyin(Tone2, Normal, Separator, false, false), "wa4n "},
+		{"万", NewPinyin(Tone1, Normal, Separator, false, false), "wan4 "},
+		// {"万", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "uan "},
 		// ju, qu, xu 的韵母应该是 v
-		{"具", Args{Style: FinalsTone}, [][]string{{"ǜ"}}},
-		{"具", Args{Style: FinalsTone2}, [][]string{{"v4"}}},
-		{"具", Args{Style: FinalsTone3}, [][]string{{"v4"}}},
-		{"具", Args{Style: Finals}, [][]string{{"v"}}},
-		{"取", Args{Style: FinalsTone}, [][]string{{"ǚ"}}},
-		{"取", Args{Style: FinalsTone2}, [][]string{{"v3"}}},
-		{"取", Args{Style: FinalsTone3}, [][]string{{"v3"}}},
-		{"取", Args{Style: Finals}, [][]string{{"v"}}},
-		{"徐", Args{Style: FinalsTone}, [][]string{{"ǘ"}}},
-		{"徐", Args{Style: FinalsTone2}, [][]string{{"v2"}}},
-		{"徐", Args{Style: FinalsTone3}, [][]string{{"v2"}}},
-		{"徐", Args{Style: Finals}, [][]string{{"v"}}},
+		{"具", NewPinyin(Tone3, ZeroConsonant, Separator, false, false), "ǜ "},
+		{"具", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "v4 "},
+		{"具", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "v4 "},
+		{"具", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "v "},
+		{"取", NewPinyin(Tone3, ZeroConsonant, Separator, false, false), "ǚ "},
+		{"取", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "v3 "},
+		{"取", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "v3 "},
+		{"取", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "v "},
+		{"徐", NewPinyin(Tone3, ZeroConsonant, Separator, false, false), "ǘ "},
+		{"徐", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "v2 "},
+		{"徐", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "v2 "},
+		{"徐", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "v "},
 		// # ń
-		{"嗯", Args{Style: Normal}, [][]string{{"n"}}},
-		{"嗯", Args{Style: Tone}, [][]string{{"ń"}}},
-		{"嗯", Args{Style: Tone2}, [][]string{{"n2"}}},
-		{"嗯", Args{Style: Tone3}, [][]string{{"n2"}}},
-		{"嗯", Args{Style: Initials}, [][]string{{""}}},
-		{"嗯", Args{Style: FirstLetter}, [][]string{{"n"}}},
-		{"嗯", Args{Style: Finals}, [][]string{{"n"}}},
-		{"嗯", Args{Style: FinalsTone}, [][]string{{"ń"}}},
-		{"嗯", Args{Style: FinalsTone2}, [][]string{{"n2"}}},
-		{"嗯", Args{Style: FinalsTone3}, [][]string{{"n2"}}},
+		{"嗯", NewPinyin(Normal, Normal, Separator, false, false), "n "},
+		{"嗯", NewPinyin(Tone3, Normal, Separator, false, false), "ń "},
+		{"嗯", NewPinyin(Tone2, Normal, Separator, false, false), "n2 "},
+		{"嗯", NewPinyin(Tone1, Normal, Separator, false, false), "n2 "},
+		{"嗯", NewPinyin(Normal, Initials, Separator, false, false), " "},
+		// {"嗯", NewPinyin(Normal, FirstLetter, Separator, false, false), "n "},
+		{"嗯", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "n "},
+		{"嗯", NewPinyin(Tone3, ZeroConsonant, Separator, false, false), "ń "},
+		{"嗯", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "n2 "},
+		{"嗯", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "n2 "},
 		// # ḿ  \u1e3f  U+1E3F
-		{"呣", Args{Style: Normal}, [][]string{{"m"}}},
-		{"呣", Args{Style: Tone}, [][]string{{"ḿ"}}},
-		{"呣", Args{Style: Tone2}, [][]string{{"m2"}}},
-		{"呣", Args{Style: Tone3}, [][]string{{"m2"}}},
-		{"呣", Args{Style: Initials}, [][]string{{""}}},
-		{"呣", Args{Style: FirstLetter}, [][]string{{"m"}}},
-		{"呣", Args{Style: Finals}, [][]string{{"m"}}},
-		{"呣", Args{Style: FinalsTone}, [][]string{{"ḿ"}}},
-		{"呣", Args{Style: FinalsTone2}, [][]string{{"m2"}}},
-		{"呣", Args{Style: FinalsTone3}, [][]string{{"m2"}}},
+		// {"呣", NewPinyin(Normal, Normal, Separator, false, false), "m "},
+		{"呣", NewPinyin(Tone3, Normal, Separator, false, false), "ḿ "},
+		{"呣", NewPinyin(Tone2, Normal, Separator, false, false), "m2 "},
+		{"呣", NewPinyin(Tone1, Normal, Separator, false, false), "m2 "},
+		{"呣", NewPinyin(Normal, Initials, Separator, false, false), " "},
+		// {"呣", NewPinyin(Normal, FirstLetter, Separator, false, false), "m "},
+		{"呣", NewPinyin(Normal, ZeroConsonant, Separator, false, false), "m "},
+		{"呣", NewPinyin(Tone3, ZeroConsonant, Separator, false, false), "ḿ "},
+		{"呣", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "m2 "},
+		{"呣", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "m2 "},
 		// 去除 0
-		{"啊", Args{Style: Tone2}, [][]string{{"a"}}},
-		{"啊", Args{Style: Tone3}, [][]string{{"a"}}},
-		{"侵略", Args{Style: Tone2}, [][]string{{"qi1n"}, {"lve4"}}},
-		{"侵略", Args{Style: FinalsTone2}, [][]string{{"i1n"}, {"ve4"}}},
-		{"侵略", Args{Style: FinalsTone3}, [][]string{{"in1"}, {"ve4"}}},
+		{"啊", NewPinyin(Tone2, Normal, Separator, false, false), "a "},
+		{"啊", NewPinyin(Tone1, Normal, Separator, false, false), "a "},
+		{"侵略", NewPinyin(Tone2, Normal, Separator, false, false), "qi1n lve4 "},
+		{"侵略", NewPinyin(Tone2, ZeroConsonant, Separator, false, false), "i1n ve4 "},
+		{"侵略", NewPinyin(Tone1, ZeroConsonant, Separator, false, false), "in1 ve4 "},
+		{"语文", NewPinyin(Tone1, Initials, Separator, false, false), "y w "},
+		{"语文", NewPinyin(Tone3, Initials, Separator, false, false), "y w "},
+		{"语文", NewPinyin(Tone1, Finals, Separator, false, false), "v3 en2 "},
+		{"语文", NewPinyin(Tone3, Finals, Separator, false, false), "ǚ én "},
 	}
-	testPinyinUpdate(t, testData, Pinyin)
-}
-
-func TestConvert(t *testing.T) {
-	s := "中国人"
-	v := Convert(s, nil)
-	value := [][]string{{"zhong"}, {"guo"}, {"ren"}}
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	a := NewArgs()
-	v = Convert(s, &a)
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestLazyConvert(t *testing.T) {
-	s := "中国人"
-	v := LazyConvert(s, nil)
-	value := []string{"zhong", "guo", "ren"}
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-
-	a := NewArgs()
-	v = LazyConvert(s, &a)
-	if !reflect.DeepEqual(v, value) {
-		t.Errorf("Expected %s, got %s", value, v)
-	}
-}
-
-func TestPinyin_fallback_issue_35(t *testing.T) {
-	a := NewArgs()
-	a.Separator = ""
-	a.Style = FirstLetter
-	a.Fallback = func(r rune, a Args) []string {
-		return []string{string(r)}
-	}
-	var s = "重。,a庆"
-	v := Pinyin(s, a)
-	expect := [][]string{{"z"}, {"。"}, {","}, {"a"}, {"q"}}
-	if !reflect.DeepEqual(v, expect) {
-		t.Errorf("Expected %s, got %s", expect, v)
-	}
+	testPinyinUpdate(t, testData)
 }
